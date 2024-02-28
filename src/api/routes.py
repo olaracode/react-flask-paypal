a567916d4fdea7f95168a9d4e5b8038dcdbc4292
Octavio Lara <octavioasapchi@gmail.com>
2024-02-28 18:54:22 -0400

    ADD paypal integration

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,15 +1,27 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import os
+import base64
+
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-
+import requests
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
+
+PAYPAL_CLIENT = os.environ.get("PAYPAL_CLIENT")
+PAYPAL_SECRET = os.environ.get("PAYPAL_SECRET")
+PAYPAL_INTENT = "CAPTURE"
+PAYPAL_APPLICATION_CONTEXT = {
+    "brand_name": "Olaracode",
+    "landing_page": "NO_PREFERENCE",
+    "use_action": "PAY_NOW"
+}
 
 
 @api.route('/hello', methods=['POST', 'GET'])
@@ -20,3 +32,61 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route("/create_order", methods=["POST"])
+def create_paypal_order():
+
+    body = request.get_json()
+    items = body.get("items", None)
+    if items is None:
+        return APIException("MISSING FIELDS", 400, {
+            "items": "The items are required to create an order"
+        }).to_dict()
+
+    total = sum(item["price"] * item["quantity"] for item in items)
+    order = {
+        "intent": PAYPAL_INTENT,
+        "application_context": PAYPAL_APPLICATION_CONTEXT,
+        "purchase_units": [
+          {
+            "amount": {
+              "currency_code": "USD",
+              "value": total,
+              "breakdown": {
+                  "item_total": {
+                      "currency_code": "USD",
+                      "value": total,
+                  }
+              }
+            },
+            "items": [
+                {
+                    "name": item["name"],
+                    "unit_amount": {
+                        "currency_code": "USD",
+                        "value": item["price"]
+                    },
+                    "quantity": item["quantity"]
+            } for item in items]
+          }
+        ]
+      }
+
+    headers = {
+        "Authorization": f"Basic {base64.b64encode(f'{PAYPAL_CLIENT}:{PAYPAL_SECRET}'.encode()).decode()}"
+    }
+
+    response = requests.post(
+          "https://api.sandbox.paypal.com/v2/checkout/orders", headers=headers, json=order
+      )
+    print(order)
+
+    if response.status_code == 201:
+        order_data = response.json()
+        return jsonify({"order_id": order_data["id"]})
+    else:
+        # Handle error and return appropriate response
+        error_data = response.json()
+        if not error_data:
+            return jsonify({"error": "There has been an error"})
+        return jsonify(error_data), response.status_code
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,25 +2,49 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 export const Home = () => {
-	const { store, actions } = useContext(Context);
-
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  const { store, actions } = useContext(Context);
+  console.log(process.env.PAYPAL_CLIENT);
+  const createOrder = async () => {
+    const response = await fetch(store.apiUrl + "/api/create_order", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        items: [
+          {
+            name: "Acrilyc mani",
+            price: 25,
+            quantity: 1,
+          },
+          {
+            name: "Pedicure",
+            price: 50,
+            quantity: 1,
+          },
+        ],
+      }),
+    });
+    if (response.ok) {
+      const data = await response.json();
+      return data.order_id;
+    }
+  };
+  return (
+    <div className="text-center mt-5">
+      <h1>PAYPAL Integration</h1>
+      <p>
+        <img src={rigoImageUrl} />
+      </p>
+      <div className="alert alert-info">
+        {store.message ||
+          "Loading message from the backend (make sure your python backend is running)..."}
+      </div>
+      <PayPalScriptProvider options={{ clientId: process.env.PAYPAL_CLIENT }}>
+        <PayPalButtons createOrder={createOrder} />
+      </PayPalScriptProvider>
+    </div>
+  );
 };
